4aac866d48601f4ebd9198b0e1536ef37b3e57a2
DevJ2K <ajavontheopro@gmail.com>
2025-05-30 14:21:26 +0200

    🏆 : End of simulation logics.

--- src/com/devj2k/avaj/launcher/utils/FtLogger.java
+++ src/com/devj2k/avaj/launcher/utils/FtLogger.java
@@ -2,32 +2,44 @@ package com.devj2k.avaj.launcher.utils;
 
 public class FtLogger {
 
-    private FtLogger() {}
+    private FtLogger() {
+    }
 
     public static void error(String description, Exception e) {
         System.out.print(Colors.to(Colors.RED, description + ": "));
         System.out.println(Colors.to(Colors.BRED, e.getMessage()));
     }
 
-    public static void parsingError(String description) {
-        System.out.print(Colors.to(Colors.RED, "Parsing Error: "));
+    public static void parsingError(String description, int line) {
+        System.out.print(Colors.to(Colors.RED, "Parsing Error (line:" + line + "): "));
         System.out.println(Colors.to(Colors.BHRED, description));
     }
 
+    // Dans simulation.txt
     public static void scenario(String aircraftType, String aircraftName, String id, String message) {
         switch (aircraftType) {
-            case "Baloon":
-                System.out.println(Colors.to(Colors.BBLUE, "Baloon#" + aircraftName + "(" + id + "): " + Colors.to(Colors.BHWHITE, message)));
-            case "Helicopter":
-                System.out.println(Colors.to(Colors.BMAG, "Helicopter#" + aircraftName + "(" + id + "): " + Colors.to(Colors.BHWHITE, message)));
-            case "JetPlane":
-                System.out.println(Colors.to(Colors.BCYAN, "JetPlane#" + aircraftName + "(" + id + "): " + Colors.to(Colors.BHWHITE, message)));
-            default:
-                System.out.println("Unknown aircraft type: " + aircraftType);
+            case "Baloon" ->
+                System.out.println(Colors.to(Colors.BBLUE,
+                        "Baloon#" + aircraftName + "(" + id + "): " + Colors.to(Colors.BHWHITE, message)));
+            case "Helicopter" ->
+                System.out.println(Colors.to(Colors.BMAG,
+                        "Helicopter#" + aircraftName + "(" + id + "): " + Colors.to(Colors.BHWHITE, message)));
+            case "JetPlane" ->
+                System.out.println(Colors.to(Colors.BCYAN,
+                        "JetPlane#" + aircraftName + "(" + id + "): " + Colors.to(Colors.BHWHITE, message)));
+            default ->
+                System.out.println("Unknown aircraft type: " + aircraftType + "#");
         }
     }
 
-    public static void tower(String message) {
-        System.out.println(Colors.to(Colors.BWHITE, "Tower says: " + Colors.to(Colors.BHWHITE, message)));
+    // Dans simulation.txt
+    public static void towerRegister(String aircraft) {
+        System.out.println(Colors.to(Colors.BWHITE,
+                "Tower says: " + Colors.to(Colors.BHWHITE, aircraft + " registred to weather tower.")));
+    }
+
+    public static void towerUnregister(String aircraft) {
+        System.out.println(Colors.to(Colors.BWHITE,
+                "Tower says: " + Colors.to(Colors.BHWHITE, aircraft + " unregistred from weather tower.")));
     }
 }
